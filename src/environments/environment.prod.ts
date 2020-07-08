5d2811f836825ec24dba28bb2fbfd6cbdd7cbd9b
Benjamin Hawkins <benjamhawk@gmail.com>
2020-07-08 18:37:40 -0500

    fix env variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://happypath-env.eba-jgedpwhn.us-east-2.elasticbeanstalk.com/'
+  apiUrl: 'http://happypath-env.eba-jgedpwhn.us-east-2.elasticbeanstalk.com'
 }
